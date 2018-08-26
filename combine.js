f8819c42dcb4d16dd74694c8ebad32fc23d0185e
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-08-26 11:37:32 +0200

    ability to add source ref

--- combine.js
+++ combine.js
@@ -7,13 +7,22 @@ const all = fs.readdirSync(path.join(__dirname, 'data'))
 program
   .version('0.0.0')
   .arguments('<filter>')
+  .option('-s, --sourceKey [name]', 'write source file name segment to this prop')
+  .option('-i, --sourceInt', 'cast source to int')
   .action((filter, cmd) => {
     console.log('filter', filter)
     const files = all.filter(string => string.startsWith(filter))
     console.log('files', files.length)
+    cmd.sourceKey && console.log('source key', cmd.sourceKey)
     const data = files.reduce(
       (agg, file) => {
         const d = require(path.join(__dirname, 'data', file))
+        if (cmd.sourceKey) {
+          d.value && d.value.forEach(v => {
+            const source = file.replace(filter, '').replace(/\.json$/, '')
+            v[cmd.sourceKey] = cmd.sourceInt ? +source : source
+          })
+        }
         agg.value = agg.value.concat(d.value)
         return agg
       },
